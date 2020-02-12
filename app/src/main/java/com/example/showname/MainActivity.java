4d37bcdea6da9713d8c226e22a80c1b4af627d66
rdowse72 <richard.dowse@gmail.com>
2020-02-12 22:56:46 +0000

    Evening changes

--- app/src/main/java/com/example/showname/MainActivity.java
+++ app/src/main/java/com/example/showname/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.showname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button myButton;
+    private TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = findViewById(R.id.button);
+        showText = findViewById(R.id.textView);
+
+        showText.setText("HELLO !!!!");
     }
 }
